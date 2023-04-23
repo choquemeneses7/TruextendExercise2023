5e6ee41e583cc1b50605810ecfe1b5bd3bc5816a
Veimar Choque <veimar.choque@truextend.com>
2023-04-23 17:09:16 -0400

    [dev] Add api services and define types

---
+++ frontend-client/src/services/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { Product, NewProduct } from '../types/Product';
+import { Category } from '../types/Category';
+
+const API_BASE_URL = 'http://localhost:3001';
+
+export async function fetchProducts(): Promise<Product[]> {
+  const response = await axios.get(`${API_BASE_URL}/products`);
+  return response.data;
+}
+
+export async function fetchCategories(): Promise<Category[]> {
+  const response = await axios.get(`${API_BASE_URL}/categories`);
+  return response.data;
+}
+
+export async function createProduct(newProduct: NewProduct): Promise<Product> {
+  const response = await axios.post(`${API_BASE_URL}/products`, newProduct);
+  return response.data;
+}
+
+export async function deleteProduct(productId: number): Promise<void> {
+  await axios.delete(`${API_BASE_URL}/products/${productId}`);
+}
---
+++ frontend-client/src/types/Product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+    id: number;
+    name: string;
+    category: string;
+    image: string;
+    price: number;
+  }
+  
+  export interface NewProduct {
+    name: string;
+    category: string;
+    image: string;
+    price: number;
+  }
+  
